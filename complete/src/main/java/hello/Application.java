0d137ca36505fea7ed839ecb81d8b54eed4753a6
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:07:49 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -18,33 +18,33 @@ import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRe
 @SuppressWarnings("unused")
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Bean
-    ApplicationRunner runner(QuoteService quoteService) {
+	@Bean
+	ApplicationRunner runner(QuoteService quoteService) {
 
-        return args -> {
-            Quote quote = requestQuote(quoteService, 12L);
-            requestQuote(quoteService, quote.getId());
-            requestQuote(quoteService, 10L);
-        };
-    }
+		return args -> {
+			Quote quote = requestQuote(quoteService, 12L);
+			requestQuote(quoteService, quote.getId());
+			requestQuote(quoteService, 10L);
+		};
+	}
 
-    private Quote requestQuote(QuoteService quoteService, Long id) {
+	private Quote requestQuote(QuoteService quoteService, Long id) {
 
-        long startTime = System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
 
-        Quote quote = Optional.ofNullable(id)
-            .map(quoteService::requestQuote)
-            .orElseGet(quoteService::requestRandomQuote);
+		Quote quote = Optional.ofNullable(id)
+			.map(quoteService::requestQuote)
+			.orElseGet(quoteService::requestRandomQuote);
 
-        long elapsedTime = System.currentTimeMillis();
+		long elapsedTime = System.currentTimeMillis();
 
-        System.out.printf("\"%1$s\"%nCache Miss [%2$s] - Elapsed Time [%3$s ms]%n", quote,
-            quoteService.isCacheMiss(), (elapsedTime - startTime));
+		System.out.printf("\"%1$s\"%nCache Miss [%2$s] - Elapsed Time [%3$s ms]%n", quote,
+			quoteService.isCacheMiss(), (elapsedTime - startTime));
 
-        return quote;
-    }
+		return quote;
+	}
 }
